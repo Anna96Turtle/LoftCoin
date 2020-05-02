a78e14d247367a49a90cb299c286b26597fda2d8
Anna <anna2.00@mail.ru>
2020-05-02 14:42:22 +0300

    macter

---
+++ app/src/main/java/com/loftblog/loftcoin/ui/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.loftblog.loftcoin.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.loftblog.loftcoin.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
